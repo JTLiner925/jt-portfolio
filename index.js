9e4767657fdfdf473d7059037e972e78248c909c
JTLiner925 <[jtliner925@gmail.com]>
2020-08-16 12:56:08 -0500

    fixed requested changes

--- index.js
+++ index.js
@@ -29,18 +29,19 @@ function startPage() {
            <section class='projects'>
            <div class ='proj'>
            <p class="appTitle"></p>
+           <p class='bible-repo repo'></p>
            <div class='appPic pic'>
            </div>
            <p class="techUsed"></p>
-           <p class='bible-repo repo'></p>
             
             </div>
             <div class ='proj'>
            <p class="prayerTitle"></p>
+           <p class='prayer-repo repo'></p>
+
            <div class='prayerPic pic'>
            </div>
            <p class="prayerTech"></p>
-           <p class='prayer-repo repo'></p>
             
             </div>
               
@@ -64,9 +65,14 @@ function startPage() {
    `;
 }
 $(window).on('load', () => {
-  const profileBio = `I live in the Austin area, but grew up in West Texas with dirt fields and mesquite trees. Now when I go outside I see rolling hills and streams nearby, it makes it easy to allow your mind to wander and be creative! Most recently, I was working as a Bakery Manager at a grocery store when I decided it was time for a change. 
-      I love thinking outside the box and working with a team to create content that is important to the client. This is a ton of fun, and it's why I got into being a developer.
-      In my spare time ...well, I don’t have any! Instead, I am happily married with three kids! We drive a mini-van, go hiking, and love Jesus!`;
+  const profileBio = `I live in the Austin area, but grew up in West Texas with dirt fields and mesquite trees. 
+  Now when I go outside I see rolling hills and streams nearby, it makes it easy to allow your mind to wander 
+  and be creative! Most recently, I was working as a Bakery Manager at a grocery store when I decided it was 
+  time for a change. With coding, the answer is always yes, if you have the imagination and determination then you can do it!
+  This really excites me, the possibilities are endless, and if you run into a hurdle you that just means you're about to learn something new. 
+  Even from a young age I knew you could achieve more if you work together and that's why I'm excited to join a team and create some awesome code.
+  In my spare time ...well, I don’t have any! Instead, I am happily married with three kids!
+  We drive a mini-van, go hiking, and love Jesus!`;
 
   $('.profile-text').text(profileBio);
 });
@@ -79,11 +85,16 @@ function activeClass(name) {
   $(`section[name=${name}]`).attr('id', 'active');
 }
 function handleProfileClick() {
-  $('.profile-hero, .Profile').on('click focus', function() {
+  $('.profile-hero, .Profile').on('click focus', function () {
     activeClass($(this).attr('name'));
-    const profileBio = `<p>I live in the Austin area, but grew up in West Texas with dirt fields and mesquite trees. Now when I go outside I see rolling hills and streams nearby, it makes it easy to allow your mind to wander and be creative! Most recently, I was working as a Bakery Manager at a grocery store when I decided it was time for a change. 
-    I love thinking outside the box and working with a team to create content that is important to the client. This is a ton of fun, and it's why I got into being a developer.
-    In my spare time ...well, I don’t have any! Instead, I am happily married with three kids! We drive a mini-van, go hiking, and love Jesus!</p>`;
+    const profileBio = `I live in the Austin area, but grew up in West Texas with dirt fields and mesquite trees. 
+    Now when I go outside I see rolling hills and streams nearby, it makes it easy to allow your mind to wander 
+    and be creative! Most recently, I was working as a Bakery Manager at a grocery store when I decided it was 
+    time for a change. With coding, the answer is always yes, if you have the imagination and determination then you can do it!
+    This really excites me, the possibilities are endless, and if you run into a hurdle you that just means you're about to learn something new. 
+    Even from a young age I knew you could achieve more if you work together and that's why I'm excited to join a team and create some awesome code.
+    In my spare time ...well, I don’t have any! Instead, I am happily married with three kids!
+    We drive a mini-van, go hiking, and love Jesus!`;
 
     $('.profile-text').html(profileBio);
 
@@ -100,11 +111,18 @@ function handleProfileClick() {
   });
 }
 function handleContactClick() {
-  $('.contact-hero, .Contact').on('click focus', function() {
+  $('.contact-hero, .Contact').on('click focus', function () {
     activeClass($(this).attr('name'));
-    const contactInfo = '<p>To contact me follow one of the links below and let\'s get started!</p>';
+    const contactInfo =
+      "<p>To contact me follow one of the links below and let's get started!</p>";
     const contactLinks = `<a href="https://www.linkedin.com/in/jt-liner-916a2a53/" aria-label="LinkedIn" tabindex='7'class="fa fa-linkedin" target="_blank"></a>
         <a href="https://github.com/JTLiner925/" aria-label="GitHub"class="fa fa-github" target="_blank"tabindex='8'></a>
+        <a
+            href="mailto:jtliner925@gmail.com"
+            aria-labelledby="email-link-label"
+            target="_blank"
+            class="fa fa-envelope"
+          ></a
        `;
 
     $('.contact-text').html(contactInfo);
@@ -121,15 +139,21 @@ function handleContactClick() {
   });
 }
 function handleProjectClick() {
-  $('.projects-hero, .Projects').on('click focus', function() {
+  $('.projects-hero, .Projects').on('click focus', function () {
     activeClass($(this).attr('name'));
-    const tech = '<p class=\'bible-description\'>oneAnother - do everything imaginable for your church small group in one location!<br>HTML/CSS<br>React.js/Node.js<br>PostgreSQL</p>';
-    const bibleRepo = '<a href="https://github.com/JTLiner925/oneanother" target="_blank"tabindex=\'4\'>GitHub Repo</a>';
-    const prayerThumb = '<a href="https://prayer-circles.vercel.app/"target="_blank"tabindex=\'5\'><img src="./icons8-church-100.png" alt=\'prayer circles\'/></a><br>';
+    const tech =
+      "<p class='bible-description'>Create a church small group, add events, read the bible together, go over questions, and sign up to bring food items to group all in one location.<br><br>HTML/CSS<br>React.js/Node.js<br>PostgreSQL</p>";
+    const bibleRepo =
+      '<a href="https://github.com/JTLiner925/oneanother" target="_blank"tabindex=\'4\'>GitHub Repo</a>';
+    const prayerThumb =
+      '<a href="https://prayer-circles.vercel.app/"target="_blank"tabindex=\'5\'><img src="./icons8-church-100.png" alt=\'prayer circles\'/></a><br>';
 
-    const appThumb = '<a href="https://oneanother.now.sh/"target="_blank"tabindex=\'3\'><img src="./icons8-holy-bible-100.ico" alt=\'one Another\'/></a><br>';
-    const prayerTech = '<p class=\'bible-description\'>Pray for your group<br>and stay connected when you\'re away!<br>HTML/CSS<br>React/Node<br>PostgreSQL<br></p>';
-    const prayerRepo = '<a href="https://github.com/JTLiner925/prayer-circles" target="_blank"tabindex=\'6\'>GitHub Repo</a>';
+    const appThumb =
+      '<a href="https://oneanother.now.sh/"target="_blank"tabindex=\'3\'><img src="./icons8-holy-bible-100.ico" alt=\'one Another\'/></a><br>';
+    const prayerTech =
+      "<p class='bible-description'>Stay connected with your group even while you're away. Create groups, events, prayer requests, and even chat with each other!<br><br>HTML/CSS<br>React/Node<br>PostgreSQL<br></p>";
+    const prayerRepo =
+      '<a href="https://github.com/JTLiner925/prayer-circles" target="_blank"tabindex=\'6\'>GitHub Repo</a>';
 
     $('.appTitle').html('oneAnother');
     $('.appPic').html(appThumb);
